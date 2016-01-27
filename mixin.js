510eaa4b8541978d36ee1f6108de1b6639a4ca1a
HuttonR <hutton.rounds@thebronson.com>
2016-01-26 23:34:29 -0800

    Cleaned up the code a bit and stopped polluting the React instance with a bunch of new private members.

--- mixin.js
+++ mixin.js
@@ -1,6 +1,8 @@
 
 ReactMeteorComputations = {
   componentWillMount() {
+    let storage = this._reactMeteorComputations = {}
+
     this._setState = this.setState
     this.setState = function (partialState, callback) {
       if (typeof partialState === 'function') partialState = partialState(this.state, this.props)
@@ -9,8 +11,8 @@ ReactMeteorComputations = {
         return this._setState(...arguments)
       }
 
-      for (let k in this._stateDependencies) {
-        let d = this._stateDependencies[k]
+      for (let k in storage._stateDependencies) {
+        let d = storage._stateDependencies[k]
         if (!d.cond || d.cond.apply(this, [partialState])) d.dep.changed()
       }
 
@@ -24,8 +26,8 @@ ReactMeteorComputations = {
         return this._replaceState(...arguments)
       }
 
-      for (let k in this._stateDependencies) {
-        let d = this._stateDependencies[k]
+      for (let k in storage._stateDependencies) {
+        let d = storage._stateDependencies[k]
         if (!d.cond || d.cond.apply(this, [nextState])) d.dep.changed()
       }
 
@@ -34,13 +36,13 @@ ReactMeteorComputations = {
 
     this._forceUpdate = this.forceUpdate
     this.forceUpdate = function (callback) {
-      for (let k in this._stateDependencies) {
-        let d = this._stateDependencies[k]
+      for (let k in storage._stateDependencies) {
+        let d = storage._stateDependencies[k]
         d.dep.changed()
       }
 
-      for (let k in this._propDependencies) {
-        let d = this._propDependencies[k]
+      for (let k in storage._propDependencies) {
+        let d = storage._propDependencies[k]
         d.dep.changed()
       }
 
@@ -49,16 +51,74 @@ ReactMeteorComputations = {
       return this._forceUpdate(...arguments)
     }
 
-    buildComps(this)
+
+
+    if (!storage._savedComputations) storage._savedComputations = []
+    if (storage._savedComputations.length) destroyComps(context)
+
+    storage._propDependencies = {}
+    storage._stateDependencies = {}
+
+    for (let name in this.computations) {
+      storage._savedComputations.push(
+        Tracker.nonreactive(() => (
+          Tracker.autorun((c) => {
+            if (!storage._nextState) {
+              // This gets the nextState so we aren't calling the computations with a "stale" state
+              storage._nextState = this._reactInternalInstance._processPendingState(this.props, this.context)
+            }
+            let savedState = this.state        // Save state
+            this.state = storage._nextState    // Set state to nextState
+            let setStateFunc = this.setState   // Save setState
+            this.setState = function () {      // Override setState
+              console.error(`\
+  You cannot call setState within a computation built by the ReactMeteorComputations mixin. \
+  This is an anti-pattern and can create infinite loops.\
+              `)
+            }
+            let res = this.computations[name].apply(this, [
+              new ReactDependency(c, this, name),
+              (dataset) => new ReturnSet(dataset)
+            ])
+            this.state = savedState            // Restore state
+            this.setState = setStateFunc       // Restore setState
+            if (! (res instanceof ReturnSet)) {
+              res = new ReturnSet({[name]: res})
+            }
+
+            for (let key in res._dataset) {
+              if (Package.mongo && Package.mongo.Mongo && res instanceof Package.mongo.Mongo.Cursor) {
+                console.warn(`\
+  Warning: you are returning a Mongo cursor from a computations function. \
+  This value will not be reactive. You probably want to call '.fetch()' \
+  on the cursor before returning it.`
+                )
+              }
+            }
+
+            this._setState(res._dataset) // Call direct
+          })
+        ))
+      )
+    }
   },
 
   componentWillUnmount() {
-    destroyComps(this)
+    let storage = this._reactMeteorComputations
+
+    for (let comp of storage._savedComputations) {
+      comp.stop()
+    }
+
+    storage._savedComputations = []
+    this._reactMeteorComputations = {}
   },
 
   componentWillReceiveProps(nextProps) {
-    for (let k in this._propDependencies) {
-      let d = this._propDependencies[k]
+    let storage = this._reactMeteorComputations
+
+    for (let k in storage._propDependencies) {
+      let d = storage._propDependencies[k]
       if (!d.cond || d.cond.apply(this, [nextProps])) d.dep.changed()
     }
 
@@ -69,66 +129,20 @@ ReactMeteorComputations = {
   },
 
   componentDidUpdate() {
-    this._nextState = undefined
+    let storage = this._reactMeteorComputations
+
+    storage._nextState = undefined
   },
 
   rerunAllComputations() {
-    for (let c of this._savedComputations) {
+    let storage = this._reactMeteorComputations
+
+    for (let c of storage._savedComputations) {
       c.invalidate()
     }
   }
 }
 
-function buildComps(context) {
-  if (!context._savedComputations) context._savedComputations = []
-  if (context._savedComputations.length) destroyComps(context)
-
-  context._propDependencies = {}
-  context._stateDependencies = {}
-
-  for (let name in context.computations) {
-    context._savedComputations.push(
-      Tracker.nonreactive(() => (
-        Tracker.autorun((c) => {
-          if (!context._nextState) {
-            // This gets the nextState so we aren't calling the computations with a "stale" state
-            context._nextState = context._reactInternalInstance._processPendingState(context.props, context.context)
-          }
-          let savedState = context.state        // Save state
-          context.state = context._nextState    // Set state to nextState
-          let setStateFunc = context.setState   // Save setState
-          context.setState = function () {      // Override setState
-            console.error(`\
-You cannot call setState within a computation built by the ReactMeteorComputations mixin. \
-This is an anti-pattern and can create infinite loops.\
-            `)
-          }
-          let res = context.computations[name].apply(context, [
-            new ReactDependency(c, context, name),
-            (dataset) => new ReturnSet(dataset)
-          ])
-          context.state = savedState            // Restore state
-          context.setState = setStateFunc       // Restore setState
-          if (! (res instanceof ReturnSet)) {
-            res = new ReturnSet({[name]: res})
-          }
-
-          for (let key in res._dataset) {
-            if (Package.mongo && Package.mongo.Mongo && res instanceof Package.mongo.Mongo.Cursor) {
-              console.warn(`\
-Warning: you are returning a Mongo cursor from a computations function. \
-This value will not be reactive. You probably want to call '.fetch()' \
-on the cursor before returning it.`
-              )
-            }
-          }
-
-          context._setState(res._dataset) // Call direct
-        })
-      ))
-    )
-  }
-}
 
 
 class ReactDependency {
@@ -139,21 +153,23 @@ class ReactDependency {
   }
 
   props(cond) {
+    let propDeps = this._context._reactMeteorComputations._propDependencies
     if (this._computation.firstRun) { // Only interested in first run, don't want to rebuild every update
       let dep = new Tracker.Dependency
-      this._context._propDependencies[this._name] = {dep, cond}
+      propDeps[this._name] = {dep, cond}
     }
 
-    this._context._propDependencies[this._name].dep.depend()
+    this._context._reactMeteorComputations._propDependencies[this._name].dep.depend()
   }
 
   state(cond) {
+    let stateDeps = this._context._reactMeteorComputations._stateDependencies
     if (this._computation.firstRun) { // Only interested in first run, don't want to rebuild every update
       let dep = new Tracker.Dependency
-      this._context._stateDependencies[this._name] = {dep, cond}
+      stateDeps[this._name] = {dep, cond}
     }
 
-    this._context._stateDependencies[this._name].dep.depend()
+    stateDeps[this._name].dep.depend()
   }
 }
 
@@ -161,12 +177,4 @@ class ReturnSet {
   constructor(dataset) {
     this._dataset = dataset
   }
-}
-
-function destroyComps(context) {
-  for (let comp of context._savedComputations) {
-    comp.stop()
-  }
-
-  context._savedComputations = []
 }
